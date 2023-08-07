e9045df13b9874113bddbe7c20a541203beed4d6
jackie <315915093@qq.com>
2023-08-08 00:13:19 +0800

    handler

--- demo1/src/evalJsCode.ts
+++ demo1/src/evalJsCode.ts
@@ -7,25 +7,28 @@ export const evalJsCode = async (
   }
 ): Promise<any> => {
   const funArgs = options?.args || [];
-  if (typeof code === "function") {
-    return await code(...funArgs);
-  }
-  let asyncFunction = new Function("return (" + code + ")");
   try {
+    if (typeof code === "function") {
+      return await code(...funArgs);
+    }
+    let asyncFunction = new Function("return (" + code + ")");
+
     return await asyncFunction()(...funArgs);
   } catch (err) {
-    console.log("执行失败", err);
+    console.log("evalJsCode 执行失败", err);
   }
 };
+
+const defaultHandler = (inputs: any[]) => inputs?.[0];
 
 // 处理允许是字符串的函数
 export const transformJsCode = (handler?: string | Function): OperatorFunction<any[], any[]> => {
   return pipe(
     !handler
-      ? map((values) => values[0]) // 默认第一个输入项作为输出
+      ? map(defaultHandler) // 默认第一个输入项作为输出
       : switchMap((values) => {
           if (handler) {
-            const result = evalJsCode(handler);
+            const result = evalJsCode(handler, { args: values });
             return from(result).pipe(
               switchMap((input) => {
                 if (isObservable(input)) {
--- demo1/src/rxFlow2.ts
+++ demo1/src/rxFlow2.ts
@@ -12,7 +12,8 @@ import {
   from,
   isObservable,
   timer,
-  take
+  take,
+  shareReplay
 } from "rxjs";
 import { evalJsCode, transformJsCode } from "./evalJsCode";
 
@@ -27,22 +28,37 @@ let nodes: Node[] = [
   {
     id: "2"
   },
-  { id: "3" },
+  {
+    id: "3",
+    handler: (x) => {
+      return of(x).pipe(
+        delay(1000),
+        map(() => "ppp")
+      );
+    }
+  },
   {
     id: "4"
     // handler: (x) => {
+    //   console.log(444444444444, x);
     //   return "y";
     // }
   },
   {
     id: "5"
+    // handler: (x) => {
+    //   return of(x).pipe(delay(2000));
+    // }
   },
   { id: "6" },
   {
-    id: "7",
-    handler: (x) => {
-      return of(x).pipe(delay(1000));
-    }
+    id: "7"
+    // handler: (x) => {
+    //   return of(x).pipe(
+    //     delay(1000),
+    //     map(() => "ppp")
+    //   );
+    // }
   }
 ];
 let edges: Edge[] = [
@@ -63,6 +79,14 @@ nodes.forEach((node) => {
   nodesMap.set(node.id, { node, subject: new ReplaySubject<any>(1), hasSubscribe: false });
 });
 
+// 找出所有的初始节点
+let startNodes = nodes.filter((node) => !edges.find((edge) => edge.targetNodeId === node.id));
+
+// 找出所有的末端节点，然后使用 forkJoin 来合并他们的流
+let endNodeSubjects = nodes
+  .filter((node) => !edges.find((edge) => edge.sourceNodeId === node.id))
+  .map((node) => nodesMap.get(node.id)!.subject);
+
 // 创建一个堆栈来存储需要处理的节点
 let stack = [...nodes];
 
@@ -74,7 +98,7 @@ while (stack.length > 0) {
 
   let upstreamNodesOfMap = edges
     .filter((edge) => edge.targetNodeId === node.id)
-    .map((edge) => nodesMap.get(edge.sourceNodeId));
+    .flatMap((edge) => nodesMap.get(edge.sourceNodeId) || []);
 
   // 如果这个节点有未订阅的上游节点，那么我们把它放回堆栈，然后继续处理下一个节点
   if (upstreamNodesOfMap.some((node) => !node!.hasSubscribe)) {
@@ -87,38 +111,35 @@ while (stack.length > 0) {
   const subscription = combineLatest(
     upstreamNodesOfMap.map((a) =>
       a!.subject.pipe(
-        tap((res) =>
-          console.log(`转换${a!.node.id} => ${node.id}`, res, {
-            node: a!.node,
-            父级: upstreamNodesOfMap,
-            handler: a?.node.handler
-          })
-        ),
-        transformJsCode(a!.node.handler)
+        tap((res) => {
+          // node.id === "4" && console.log("idddd=4", res);
+          // console.log(`【转换】${a!.node.id} => ${node.id}，使用id=${node.id}的handler`, res);
+        })
       )
     )
   )
     .pipe(
-      //   shareReplay(1),
+      tap((res) => console.log(`【转换】使用id=${node.id}的handler`, res)),
+      transformJsCode(node.handler), // 上游来值，用当前的handler直接处理掉
+      tap((res) => {
+        nodesMap.get(node.id)!.subject.next(res);
+      }),
+      // shareReplay(1),
       catchError((err) => {
         console.log(err);
         return EMPTY;
       })
     )
     .subscribe((res) => {
-      console.log(`id=${node.id} 得到`, res, { 父级: upstreamNodesOfMap });
-      nodesMap.get(node.id)!.subject.next(upstreamNodesOfMap.length === 1 ? res[0] : res);
+      // console.log(`id=${node.id} 得到`, res, { 父级: upstreamNodesOfMap });
+      // node.id == "4" && console.log("id=4", { node, upstreamNodesOfMap });
+      // nodesMap.get(node.id)!.subject.next(upstreamNodesOfMap.length === 1 ? res[0] : res);
     });
 
   subscriptions.add(subscription);
 
   nodesMap.get(node.id)!.hasSubscribe = true;
 }
-
-// 找出所有的末端节点，然后使用 forkJoin 来合并他们的流
-let endNodeSubjects = nodes
-  .filter((node) => !edges.find((edge) => edge.sourceNodeId === node.id))
-  .map((node) => nodesMap.get(node.id)!.subject);
 
 combineLatest(endNodeSubjects)
   .pipe(
@@ -134,16 +155,23 @@ combineLatest(endNodeSubjects)
 // 对于源头节点，开始数据流
 nodes.forEach((node) => {
   if (!edges.some((edge) => edge.targetNodeId === node.id)) {
-    console.log(`源头节点`, node.id);
-    nodesMap.get(node.id)!.subject.next("x");
+    // console.log(`源头节点`, node.id);
+    of(["x"])
+      .pipe(
+        tap((res) => console.log(`【转换】{源头}使用id=${node.id}的handler`, res)),
+        transformJsCode(node.handler)
+      )
+      .subscribe((res) => {
+        nodesMap.get(node.id)!.subject.next(res);
+      });
   }
 });
 
 // 在需要取消订阅的时候
 //subscriptions.unsubscribe();
 
-timer(0, 1000)
-  .pipe(take(5))
-  .subscribe((res) => {
-    console.log(res);
-  });
+// timer(0, 1000)
+//   .pipe(take(5))
+//   .subscribe((res) => {
+//     console.log(res);
+//   });
